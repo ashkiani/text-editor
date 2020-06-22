b399cffbc9a3da5bff29e86d9c25940089fe2226
ashkiani <ashkiani@yahoo.com>
2020-06-22 10:12:15 -0400

    Added textarea for content

--- client/src/components/OpenFile/index.jsx
+++ client/src/components/OpenFile/index.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState }  from 'react'
 
-export default function index(props) {
+export default function Index(props) {
+    const [content, setContent] = useState([props.file.content]);
+    function handleChange(event) {
+        const { value } = event.target;
+        setContent(value);
+    }
     return (
         <div>
             <div className="border" style={{ height: '250px', backgroundColor: 'yellow' }}>This is the opened file! {props.file.id}
                 <button key={props.id} className="float-right" onClick={() => props.closeFileFunction(props.file)}>Close</button>
                 <div>
-                    {props.file.content}
+                    <textarea id="noter-text-area" name="textarea" value={content} onChange={handleChange} />
                 </div>
             </div>
         </div>
