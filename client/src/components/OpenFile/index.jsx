82c95fe15e52225d1701037822eb2ebdcd9f60f1
ashkiani <ashkiani@yahoo.com>
2020-06-22 10:41:59 -0400

    Added a Save button (Under Construction)

--- client/src/components/OpenFile/index.jsx
+++ client/src/components/OpenFile/index.jsx
@@ -1,20 +1,41 @@
-import React, { useState }  from 'react'
+import React, { useState } from 'react'
 
 export default function Index(props) {
-    const [content, setContent] = useState([props.file.content]);
+    const [content, setContent] = useState({ initialContent: props.file.content, currentContent: props.file.content });
     function handleChange(event) {
         const { value } = event.target;
-        setContent(value);
+        setContent({ initialContent: props.file.content, currentContent: value });
     }
-    return (
-        <div>
-            <div className="border" style={{ height: '250px', backgroundColor: 'yellow' }}>This is the opened file! {props.file.id}
-                <button key={props.id} className="float-right" onClick={() => props.closeFileFunction(props.file)}>Close</button>
-                <div>
-                    <textarea id="noter-text-area" name="textarea" value={content} onChange={handleChange} />
+    function saveClick() {
+        alert("Saving content for " + props.file.id);
+    }
+    if (content.currentContent === content.initialContent) {
+        return (
+            <div>
+                <div className="border" style={{ height: '250px', backgroundColor: 'yellow' }}>This is the opened file! {props.file.id}
+                    <button key={props.id} className="float-right" onClick={() => props.closeFileFunction(props.file)}>Close</button>
+                    <div>
+                        <textarea name="content" className="w-100" value={content.currentContent} onChange={handleChange} />
+                    </div>
                 </div>
             </div>
-        </div>
 
-    )
+        )
+    }
+    else {
+        return (
+            <div>
+                <div className="border" style={{ height: '250px', backgroundColor: 'yellow' }}>This is the opened file! {props.file.id}
+                    <button key={props.id} className="float-right" onClick={() => props.closeFileFunction(props.file)}>Close</button>
+                    <div>
+                        <textarea name="content" className="w-100" value={content.currentContent} onChange={handleChange} />
+                    </div>
+                    <div>
+                        <button onClick={saveClick}>Save</button>
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
 }
