7bb27aa48f2aeaa28e8438969e30eaf86c3a41c6
ashkiani <ashkiani@yahoo.com>
2020-06-22 18:03:47 -0400

    Converted OpenFile into a class component

--- client/src/components/OpenFile/index.jsx
+++ client/src/components/OpenFile/index.jsx
@@ -1,41 +1,100 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 
-export default function Index(props) {
-    const [content, setContent] = useState({ initialContent: props.file.content, currentContent: props.file.content });
-    function handleChange(event) {
+export default class index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            initialContent: "",
+            currentContent: ""
+        };
+        this.handleChange = this.handleChange.bind(this)
+    }
+ 
+    handleChange = (event) => {
         const { value } = event.target;
-        setContent({ initialContent: props.file.content, currentContent: value });
+        this.setState({
+            initialContent: this.state.initialContent,
+            currentContent: value
+        });
     }
-    function saveClick() {
-        alert("Saving content for " + props.file.id);
+
+    saveClick = () => {
+        fetch("/api/files/content", {
+            method: "PUT",
+            body: JSON.stringify({ fileId: this.props.file.id, content: this.state.currentContent }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(async res => {
+            if (res.status === 200) {
+                this.setState({
+                    initialContent: this.state.currentContent,
+                    currentContent: this.state.currentContent
+                });
+                // setContent({ initialContent: content.currentContent, currentContent: content.currentContent });
+            } else {
+                res.text().then(text => { alert("Error please try again -" + text) });
+            }
+        })
+            .catch(err => {
+                console.log(err);
+                alert("Error deleting file please try again");
+            });
     }
-    if (content.currentContent === content.initialContent) {
-        return (
-            <div>
-                <div className="border" style={{ height: '250px', backgroundColor: 'yellow' }}>This is the opened file! {props.file.id}
-                    <button key={props.id} className="float-right" onClick={() => props.closeFileFunction(props.file)}>Close</button>
-                    <div>
-                        <textarea name="content" className="w-100" value={content.currentContent} onChange={handleChange} />
+  
+    componentDidMount() {
+        fetch("/api/files/content", {
+            method: "POST",
+            body: JSON.stringify({ fileId: this.props.file.id }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(async res => {
+            if (res.status === 200) {
+                let data = await res.json();
+                console.log(data);
+                this.setState({
+                    initialContent: data.content,
+                    currentContent: data.content
+                });
+            } else {
+                res.text().then(text => { alert("Error please try again -" + text) });
+            }
+        })
+            .catch(err => {
+                console.log(err);
+                alert("Error deleting file please try again");
+            });
+    }
+    render() {
+        if (this.state.currentContent === this.state.initialContent) {
+            return (
+                <div>
+                    <div className="border" style={{ height: '250px', backgroundColor: 'yellow' }}>This is the opened file! {this.props.file.id}
+                        <button key={this.props.id} className="float-right" onClick={() => this.props.closeFileFunction(this.props.file)}>Close</button>
+                        <div>
+                            <textarea name="content" className="w-100" value={this.state.currentContent} onChange={this.handleChange} />
+                        </div>
                     </div>
                 </div>
-            </div>
 
-        )
-    }
-    else {
-        return (
-            <div>
-                <div className="border" style={{ height: '250px', backgroundColor: 'yellow' }}>This is the opened file! {props.file.id}
-                    <button key={props.id} className="float-right" onClick={() => props.closeFileFunction(props.file)}>Close</button>
-                    <div>
-                        <textarea name="content" className="w-100" value={content.currentContent} onChange={handleChange} />
-                    </div>
-                    <div>
-                        <button onClick={saveClick}>Save</button>
+            )
+        }
+        else {
+            return (
+                <div>
+                    <div className="border" style={{ height: '250px', backgroundColor: 'yellow' }}>This is the opened file! {this.props.file.id}
+                        <button key={this.props.id} className="float-right" onClick={() => this.props.closeFileFunction(this.props.file)}>Close</button>
+                        <div>
+                            <textarea name="content" className="w-100" value={this.state.currentContent} onChange={this.handleChange} />
+                        </div>
+                        <div>
+                            <button onClick={this.saveClick}>Save</button>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-        )
+            )
+        }
     }
 }
