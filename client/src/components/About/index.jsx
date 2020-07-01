96287c180e5f114489ae27c3655751936f041452
ashkiani <ashkiani@yahoo.com>
2020-07-01 19:52:25 -0400

    Updated the About page

--- client/src/components/About/index.jsx
+++ client/src/components/About/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-export default function index() {
+export default function Index() {
     return (
         <div>
             <div className="container">
                 <h1>About</h1>
+                <p><strong>Siavash Notepad -Text Editor App</strong> is a simple online text editor app which also allows users to share their note/text files.</p>
+                <p>The app is open source and built on the <a href="https://github.com/ashkiani/file-manager-auth-mern" target="_blank" rel="noopener noreferrer">file-manager-auth-mern</a> template, using the <a href="https://www.npmjs.com/package/create-file-manager-app" target="_blank" rel="noopener noreferrer">create-file-manager-app</a> tool</p>
+                <p>The source code is available here:<a href="https://github.com/ashkiani/text-editor" target="_blank" rel="noopener noreferrer">https://github.com/ashkiani/text-editor</a></p>
             </div>
         </div>
     )
