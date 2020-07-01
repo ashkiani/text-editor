bd19854d05311b744e0d80e0808b0bff4ed16da1
ashkiani <ashkiani@yahoo.com>
2020-07-01 19:53:20 -0400

    minor change in the main link

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -8,7 +8,7 @@ export default function index() {
         <nav className="navbar navbar-light navbar-expand-md bg-faded justify-content-center  bg-info text-white my-2">
           <div className="p-2 w-90">
             <NavLink className="nav-item" to="/app">
-              <button type="button" className="btn btn-link text-light"><strong>Siavash Text Editor App</strong> <i className="fa fa-pencil-square-o" aria-hidden="true"> </i> </button>
+              <button type="button" className="btn btn-link text-light"><strong>Siavash Notepad -Text Editor App</strong> <i className="fa fa-pencil-square-o" aria-hidden="true"> </i> </button>
             </NavLink>
           </div>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar3">
