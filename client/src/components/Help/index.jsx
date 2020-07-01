e0479577f88191bb2492992b58dc948ebab826d1
ashkiani <ashkiani@yahoo.com>
2020-07-01 19:52:58 -0400

    Updated Help page

--- client/src/components/Help/index.jsx
+++ client/src/components/Help/index.jsx
@@ -4,6 +4,13 @@ export default function index() {
     return (
         <div className="container">
             <h1>Help</h1>
+            <ol>
+                <li>Click the <strong>Siavash Notepad -Text Editor App</strong> link in the navbar </li>
+                <li>Create Account or Login</li>
+                <li>Create files by clicking the <i className="fa fa-plus-square" aria-hidden="true"></i> button</li>
+            </ol>
+            <p>Now you can edit, delete or share the file using the available buttons.</p>
+            <p>If you still have questions send us an email at <u>ashkiani@yahoo.com</u></p>
         </div>
     )
 }
